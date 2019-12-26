2c1bcef7860c7e5edda346c87e59d81c65fe3d2f
akshansh1998 <ankushanand718@gmail.com>
2019-12-26 15:50:55 +0530

    Stable Version 1.0
    Removed All Dependencies and Stuff

--- dd-maintenance-mode.php
+++ dd-maintenance-mode.php
@@ -2,35 +2,66 @@
 /*
  * Plugin Name: DD Simplest Maintenance Mode
  * Plugin URI: https://github.com/akshansh1998/dd-simplest-maintenance
- * Description: Displays a maintenance mode page for anyone who's not logged in.
+ * Description: A Light Weight Maintenance Plugin, as Plugin is Activated, It Displays a maintenance mode page for anyone who's not logged in.  
  * Version: 1.0
- * Author: Ankush anand
- * Author URI: https://ankushanand.com
- * License: GNU GPL3
+ * Author: Ankush Anand
+ * Author URI: https://github.com/akshansh1998
+ * License: GPLv2 or later
  *
- * @package dd-maintenance-mode
+ * @package dd-simplest-maintenance-mode
  * @copyright Copyright (c) 2020, Ankush Anand
- * @license GNU GPL3
+
+
 */
 
 /**
- * Simplest Maintenance Page
+ * DD Simplest Maintenance Mode
  *
  * Displays the coming soon page for anyone who's not logged in.
  * The login page gets excluded so that you can login if necessary.
  *
  * @return void
  */
-function dd_maintenance_mode() {
+
+// Main Function of Plugin
+
+function dd_maintenance_mode()
+{
 	global $pagenow;
-	if ( $pagenow !== 'wp-login.php' && ! current_user_can( 'manage_options' ) && ! is_admin() ) {
-		header( 'HTTP/1.1 Service Unavailable', true, 503 );
-		header( 'Content-Type: text/html; charset=utf-8' );
-		if ( file_exists( plugin_dir_path( __FILE__ ) . 'views/maintenance.php' ) ) {
-			require_once( plugin_dir_path( __FILE__ ) . 'views/maintenance.php' );
+	if ($pagenow !== 'wp-login.php' && !current_user_can('manage_options') && !is_admin()) {
+		header('HTTP/1.1 Service Unavailable', true, 503);
+		header('Content-Type: text/html; charset=utf-8');
+		if (file_exists(plugin_dir_path(__FILE__) . 'views/maintenance.php')) {
+			require_once(plugin_dir_path(__FILE__) . 'views/maintenance.php');
 		}
 		die();
 	}
 }
 
-add_action( 'wp_loaded', 'dd_maintenance_mode' );
+// Adding Link
+add_filter('plugin_row_meta', 'ddDonate', 10, 2);
+
+function ddDonate($links, $file)
+{
+	if (plugin_basename(__FILE__) == $file) {
+		$row_meta = array(
+			'Support Author'    => '<a href="' . esc_url('https://ankushanand.com/donate/') . '" target="_blank" aria-label="' . esc_attr__('Plugin Additional Links', 'domain') . '" style="color:green;">' . esc_html__('Support Author', 'domain') . '</a>'
+		);
+
+		return array_merge($links, $row_meta);
+	}
+	return (array) $links;
+}
+
+// Admin Notice - Maintenance Mode is Active
+
+function sample_admin_notice__success()
+{
+?>
+	<div class="notice notice-success">
+		<p><?php _e('Maintenance Mode is <b>Active</b>!', 'sample-text-domain'); ?></p>
+	</div>
+<?php
+}
+add_action('admin_notices', 'sample_admin_notice__success');
+add_action('wp_loaded', 'dd_maintenance_mode');
